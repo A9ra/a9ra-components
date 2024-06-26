e62cdaeb3f0c062f9b189ff0721c665bd408f8d8
Youcef Madadi <tahher@live.fr>
2024-06-26 20:11:28 +0100

    refactor: Add random colors to subjects in subjectsMap

--- validation/models/enums/subjects.ts
+++ validation/models/enums/subjects.ts
@@ -1,78 +1,84 @@
 import { z } from '^common/defaultZod';
 
+import { generateRandomColor } from './tools';
+
 export const subjectsMap: Record<subjectsEnumT, EnumsDetailsI<subjectsEnumT>> = {
 	Math: {
 		id: 'Math',
 		name: 'Math',
-		icon: 'Math',
+		icon: 'icon-[mdi--book]',
 	},
 	Science: {
 		id: 'Science',
 		name: 'Science',
-		icon: 'Science',
+		icon: 'icon-[mdi--flask]',
 	},
 	English: {
 		id: 'English',
 		name: 'English',
-		icon: 'English',
+		icon: 'icon-[mdi--translate]',
 	},
 	History: {
 		id: 'History',
 		name: 'History',
-		icon: 'History',
+		icon: 'icon-[mdi--history]',
 	},
 	'Islamic Studies': {
 		id: 'Islamic Studies',
 		name: 'Islamic Studies',
-		icon: 'Islamic Studies',
+		icon: 'icon-[mdi--moon-and-stars]',
 	},
 	Art: {
 		id: 'Art',
 		name: 'Art',
-		icon: 'Art',
+		icon: 'icon-[mdi--palette]',
 	},
 	Music: {
 		id: 'Music',
 		name: 'Music',
-		icon: 'Music',
+		icon: 'icon-[mdi--music]',
 	},
 	'Physical Education': {
 		id: 'Physical Education',
 		name: 'Physical Education',
-		icon: 'Physical Education',
+		icon: 'icon-[mdi--dumbbell]',
 	},
 	'Computer Science': {
 		id: 'Computer Science',
 		name: 'Computer Science',
-		icon: 'Computer Science',
+		icon: 'icon-[mdi--laptop]',
 	},
 	'Social Studies': {
 		id: 'Social Studies',
 		name: 'Social Studies',
-		icon: 'Social Studies',
+		icon: 'icon-[mdi--earth]',
 	},
 	Arabic: {
 		id: 'Arabic',
 		name: 'Arabic',
-		icon: 'Arabic',
+		icon: 'icon-[mdi--translate]',
 	},
 	French: {
 		id: 'French',
 		name: 'French',
-		icon: 'French',
+		icon: 'icon-[mdi--translate]',
 	},
 	German: {
 		id: 'German',
 		name: 'German',
-		icon: 'German',
+		icon: 'icon-[mdi--translate]',
 	},
 	Spanish: {
 		id: 'Spanish',
 		name: 'Spanish',
-		icon: 'Spanish',
+		icon: 'icon-[mdi--translate]',
 	},
 };
 export const subjectsList = Object.keys(subjectsMap) as unknown as MyEnum<subjectsEnumT>;
+export const subjectsWithColorsList = subjectsList.map((elm, i) => ({
+	...subjectsMap[elm],
+	color: generateRandomColor(i),
+}));
 
 export const subjectsEnumSchema = (error?: ErrorsSchemaMsgI) =>
 	z
