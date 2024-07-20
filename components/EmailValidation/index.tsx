2228698f37d892b6cbcc0c5891f360761e68b9fe
Youcef Madadi <tahher@live.fr>
2024-07-20 02:37:21 +0100

    refactor: Update EmailValidation component to use react-query for resend validation

--- components/EmailValidation/index.tsx
+++ components/EmailValidation/index.tsx
@@ -1,42 +1,43 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
-import { useResendValidationMutation } from '$client/hooks/auth';
+import { resendValidation } from '$client/endpoints/auth';
 
 import useUser from ':common/useUser';
 
 import CountDownValidationButton from './CountDownValidationButton';
-
 export default function EmailValidation() {
 	const [sentAt, setSentAt] = React.useState(new Date(1));
 	const { user } = useUser();
 	const navigate = useNavigate();
-	const [RequestValidate, { isLoading }] = useResendValidationMutation();
-	const ResendValidation = useCallback(() => {
-		// send email
-		if (!user || user.emailValidated) return;
-		RequestValidate({
-			email: user.email,
-		})
-			.unwrap()
-			.then((res) => {
-				toast.success('Email sent');
-				setSentAt(new Date());
-				navigate('/validate/email?sessionId=' + res.data);
-			})
-			.catch(() => {
-				toast.error('Failed to send email');
-			});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user]);
+	const { mutate, isPending: isLoading } = useMutation({
+		mutationFn: resendValidation,
+		onSuccess: (res) => {
+			toast.success('Email sent');
+			setSentAt(new Date());
+			navigate('/validate/email?sessionId=' + res.data);
+		},
+		onError: () => {
+			toast.error('Failed to send email');
+		},
+		mutationKey: ['resendValidation'],
+	});
+
 	if (!user || user.emailValidated) return null;
 	return (
 		<div className="flex w-full items-center justify-center gap-8 border-b px-4 py-2 shadow">
 			<p className="text-sm">
 				Your have not validated your email yet. If you did not receive the email, please check your spam folder.
 			</p>
-			<CountDownValidationButton sentAt={sentAt} onClick={ResendValidation} isLoading={isLoading} />
+			<CountDownValidationButton
+				sentAt={sentAt}
+				onClick={() => {
+					mutate({ email: user.email });
+				}}
+				isLoading={isLoading}
+			/>
 		</div>
 	);
 }
