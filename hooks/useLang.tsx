52fa16092a0ec032ed5ff0c72f2c9042f01ae968
Abdelhak Ihadjadene <abdelhakihadjadene@gmail.com>
2024-07-23 20:15:39 +0100

    Added GA events for language

--- hooks/useLang.tsx
+++ hooks/useLang.tsx
@@ -1,5 +1,8 @@
+import ReactGA from 'react-ga4';
+
 import { useAppDispatch, useAppSelector } from ':common/redux';
 import { languages, setLang } from '@common/contexts/language';
+
 export const AppLanguages: Record<LanguagesI, string> = {
 	EN: 'English (🇺🇸)',
 	FR: 'Français (🇫🇷)',
@@ -9,7 +12,14 @@ export default function useLang() {
 	const dispatch = useAppDispatch();
 	const language = useAppSelector((state) => state.language).language;
 	const set = (lang: LanguagesI) => {
-		if (languages.includes(lang)) dispatch(setLang(lang));
+		if (languages.includes(lang)) {
+			ReactGA.event({
+				category: 'Language',
+				action: 'Change in language',
+				label: `${AppLanguages[lang]}`,
+			});
+			dispatch(setLang(lang));
+		}
 	};
 	return { setLanguage: set, language };
 }
